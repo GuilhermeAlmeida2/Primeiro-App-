0fcde80225c225ba2ad2a2c1bf4ce489c9e4b703
Guilherme Almeida <guilhermeamorimasilva@gmail.com>
2024-03-29 21:36:10 -0300

    Modificação de nomes de arquivos e adição de botão

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,124 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 main() {
-  runApp(PrimeiroApp());
+  runApp(SegundoApp());
 }
 
-class PrimeiroApp extends StatelessWidget {
+class SegundoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: "Meu Primeiro App Flutter",
-      home: HomePage(),
+      title: "Home Page com Flutter",
+      home: TelaInicio(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({
-    super.key,
-  });
+class TelaInicio extends StatelessWidget {
+  const TelaInicio({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("HomePage"),
-        backgroundColor: Colors.orange,
-        leading: const Icon(
-          Icons.person,
-        ),
+        title: const Text("Home Page",
+            style: TextStyle(color: Colors.white, fontSize: 28)),
+        backgroundColor: Color(0xFF7D60A1),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Texto4"),
-              Text("Texto5"),
-              Text("Texto6"),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Every Purchase\n Will be Made\n With Pleasure",
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                  SizedBox(height: 5),
+                  Text("Buy and Enjoy",
+                      style: TextStyle(fontSize: 18.69),
+                      textAlign: TextAlign.center),
+                ],
+              ),
             ],
           ),
-          ElevatedButton(
-            onPressed: () {
-              print('Botão clicado');
-            },
-            child: const Text('Primeiro Botão'),
+          Column(
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+
+                // Button 1
+                child: ElevatedButton(
+                  onPressed: () {
+                    print('Botão clicado');
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF7D60A1),
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.only(
+                        top: 16,
+                        left: 25,
+                        bottom: 16,
+                        right: 25,
+                      )),
+                  child: const Text(
+                    "Start Shopping",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+              const Padding(
+                /* Bottom de butoon */
+                padding: EdgeInsets.only(bottom: 20),
+              ),
+
+              // Button 2
+              ElevatedButton(
+                onPressed: () {
+                  print("Learn More");
+                },
+                style: ElevatedButton.styleFrom(
+                  alignment: Alignment.centerLeft,
+                  
+                ),
+                child: const Text(
+                  "Learn More",
+                ),
+              ),
+              const Padding(
+                /* Bottom de butoon */
+                padding: EdgeInsets.only(bottom: 20),
+              ),
+
+              // Button 3
+              ElevatedButton(
+                onPressed: () {
+                  print("Login");
+                },
+                style: ElevatedButton.styleFrom(
+                  alignment: Alignment.topRight,
+                ),
+                child: const Text(
+                  "Login",
+                ),
+              ),
+              const Padding(
+                /* Bottom de butoon */
+                padding: EdgeInsets.only(bottom: 20),
+              ),
+            ],
           ),
-          const Padding(
-            padding: EdgeInsets.only(top: 24, bottom: 4, left: 16, right: 87),
-            child: Icon(
-              Icons.home,
-            ),
-          ),
+          //top
+
+          // Buttom 2
         ],
       ),
     );
