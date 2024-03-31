5a1215c389f1d24d044de05cd387a172b372eac7
Guilherme Almeida <guilhermeamorimasilva@gmail.com>
2024-03-31 14:29:25 -0300

    Implementação: Alteração da cor dos botões.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 
 main() {
   runApp(SegundoApp());
@@ -18,7 +15,7 @@ class SegundoApp extends StatelessWidget {
 }
 
 class TelaInicio extends StatelessWidget {
-  const TelaInicio({super.key});
+  const TelaInicio({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,99 +23,120 @@ class TelaInicio extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Home Page",
             style: TextStyle(color: Colors.white, fontSize: 28)),
-        backgroundColor: Color(0xFF7D60A1),
+        backgroundColor: const Color(0xFF7D60A1),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          const Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Every Purchase\n Will be Made\n With Pleasure",
-                      textAlign: TextAlign.center,
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                  SizedBox(height: 5),
-                  Text("Buy and Enjoy",
-                      style: TextStyle(fontSize: 18.69),
-                      textAlign: TextAlign.center),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("Every Purchase\n Will be Made\n With Pleasure",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold)),
+                      SizedBox(height: 5),
+                      Text("Buy and Enjoy",
+                          style: TextStyle(fontSize: 18.69),
+                          textAlign: TextAlign.center),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 15),
+
+                    // Button 1
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print('Botão clicado');
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF7D60A1),
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.only(
+                              top: 16, left: 25, bottom: 16, right: 25)),
+                      child: const Text(
+                        "Start Shopping",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 20),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Button 2
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Ir para Learn More");
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        side: BorderSide(color: Colors.black),
+                        alignment: Alignment.centerLeft),
+                    child: const Text(
+                      "Learn More",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 20),
+                  ),
+
+                  // Button 3
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Ir para Login");
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        side: BorderSide(color: Colors.black),
+                        alignment: Alignment.centerRight),
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
                 ],
               ),
             ],
           ),
-          Column(
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 15),
-
-                // Button 1
-                child: ElevatedButton(
-                  onPressed: () {
-                    print('Botão clicado');
-                  },
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF7D60A1),
-                      alignment: Alignment.center,
-                      padding: const EdgeInsets.only(
-                        top: 16,
-                        left: 25,
-                        bottom: 16,
-                        right: 25,
-                      )),
-                  child: const Text(
-                    "Start Shopping",
-                    style: TextStyle(color: Colors.white, fontSize: 25),
-                  ),
-                ),
+          Positioned(
+            bottom: 10.0,
+            right: 16.0,
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: const Color(0xFF7D60A1),
               ),
-              const Padding(
-                /* Bottom de butoon */
-                padding: EdgeInsets.only(bottom: 20),
-              ),
-
-              // Button 2
-              ElevatedButton(
+              child: IconButton(
                 onPressed: () {
-                  print("Learn More");
+                  print("Voltar para o Home Page");
                 },
-                style: ElevatedButton.styleFrom(
-                  alignment: Alignment.centerLeft,
-                  
-                ),
-                child: const Text(
-                  "Learn More",
-                ),
+                icon: Icon(Icons.home, color: Colors.white),
+                iconSize: 35,
               ),
-              const Padding(
-                /* Bottom de butoon */
-                padding: EdgeInsets.only(bottom: 20),
-              ),
-
-              // Button 3
-              ElevatedButton(
-                onPressed: () {
-                  print("Login");
-                },
-                style: ElevatedButton.styleFrom(
-                  alignment: Alignment.topRight,
-                ),
-                child: const Text(
-                  "Login",
-                ),
-              ),
-              const Padding(
-                /* Bottom de butoon */
-                padding: EdgeInsets.only(bottom: 20),
-              ),
-            ],
+            ),
           ),
-          //top
-
-          // Buttom 2
         ],
       ),
     );
